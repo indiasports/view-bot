db2c40f0eb43c30db41f7e840eb0fe02707db5b8
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-07-17 16:54:11 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 agent = ua.random
 
-
-url ='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FRojanGamingYT%2FRojanGamingYT'
-
-urll = 'https://l.instagram.com/?u=https%3A%2F%2Frojansapkota.com.np%2F&e=ATOOyL2K5Q0S91Mk-jqZPvf5UY-xewGCP2RGEtcxXhJWnN_VZOirdnVBPlyHDwfCQg0AuQuUG2cVKHijBljlUg&s=1'
+url = 'https://l.instagram.com/?u=https%3A%2F%2Frojansapkota.com.np%2F&e=ATOOyL2K5Q0S91Mk-jqZPvf5UY-xewGCP2RGEtcxXhJWnN_VZOirdnVBPlyHDwfCQg0AuQuUG2cVKHijBljlUg&s=1'
 
 headers = {
     "User-Agent":
@@ -16,7 +13,7 @@ headers = {
 
 def requests_sender():
 	while True:
-		requests.get(url, headers=headers,timeout=(3.05, 2))
+		requests.get(url, headers=headers,timeout=(4,2))
 		agent = ua.random
 		print(f"Request Sent as {agent}")
 
