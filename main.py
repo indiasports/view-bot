ef3753f6d4ce7efccf95c258ae41271f0961fdb9
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-07-17 21:40:57 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 agent = ua.random
 
-url = 'https://l.instagram.com/?u=https%3A%2F%2Frojansapkota.com.np%2F&e=ATOOyL2K5Q0S91Mk-jqZPvf5UY-xewGCP2RGEtcxXhJWnN_VZOirdnVBPlyHDwfCQg0AuQuUG2cVKHijBljlUg&s=1'
+url = 'https://www.neppixel.xyz/threads/how-to-rank-1-in-google-search-engine.3/#post-5'
+urll = 'https://l.instagram.com/?u=https%3A%2F%2Frojansapkota.com.np%2F&e=ATOOyL2K5Q0S91Mk-jqZPvf5UY-xewGCP2RGEtcxXhJWnN_VZOirdnVBPlyHDwfCQg0AuQuUG2cVKHijBljlUg&s=1'
 
 headers = {
     "User-Agent":
