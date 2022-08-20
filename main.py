f85d477eb39c7f06d2d297caa9e936475f16b48e
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-08-20 09:41:04 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 agent = ua.random
 
-url = 'https://rojansapkota.com.np'
+url = 'https://www.google.com/url?sa=t&source=web&rct=j&url=https://rojansapkota.com.np/&ved=2ahUKEwjoz-jWwtT5AhWjR2wGHfMJC10QFnoECAsQAQ&usg=AOvVaw1_l83Z7fWhL-Xh5GNY8ukh'
 
 headers = {
     "User-Agent":
