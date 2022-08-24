5fc64fcd1564db4b6f016f59517c3fa2cb2bf032
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-08-24 14:47:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 agent = ua.random
 
-url = 'https://rojansapkota.com.np'
+url = 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=rojansapkota.com.np&count_bg=%2379C83D&title_bg=%23555555&icon=v.svg&icon_color=%234ACCC8&title=Website+Views&edge_flat=true'
 urll = 'https://www.google.com/url?sa=t&source=web&rct=j&url=https://rojansapkota.com.np/&ved=2ahUKEwjoz-jWwtT5AhWjR2wGHfMJC10QFnoECAsQAQ&usg=AOvVaw1_l83Z7fWhL-Xh5GNY8ukh'
 
 headers = {
