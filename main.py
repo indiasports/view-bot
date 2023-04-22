95f8824badd5b2c4435a36394539bedba8980ba3
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2023-04-22 19:54:11 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import requests, threading, time
 from fake_useragent import UserAgent
 
-ua = UserAgent()
-agent = ua.random
-
 url = 'https://dsc.gg/joindedsec'
-
-headers = {
-    "User-Agent":
-    agent
-}
 
 def requests_sender():
 	while True:
-		requests.get(url, headers=headers)
+		ua = UserAgent()
 		agent = ua.random
+		headers = {
+    "User-Agent":
+    agent
+}
+		requests.get(url, headers=headers)
 		print(f"Request Sent as {agent}")
 
 if __name__ == "__main__":
